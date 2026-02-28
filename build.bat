59755b533cf2fd58df860457b3f4afec68b4d946
Rev Oconner <oconnerrev@gmail.com>
2026-02-28 22:48:46 +0530

    remove messenger and updated build,bat

--- build.bat
+++ build.bat
@@ -12,7 +12,3 @@ echo Building executable...
 clang++ -O3 -Wall -Wextra -std=c++17 -fno-exceptions -I include -o headless-tty.exe src/pty.cpp src/main.cpp resources/app.res -static -luser32 -lshell32 -Wl,/SUBSYSTEM:WINDOWS -Wl,/ENTRY:mainCRTStartup
 
 if %ERRORLEVEL%==0 echo Build successful
-
-@REM echo Building helper...
-@REM g++ -std=c++23 -o messenger.exe Helper/messenger.cpp -static -s -mwindows -lbcrypt
-@REM if %ERRORLEVEL%==0 echo Build successful
