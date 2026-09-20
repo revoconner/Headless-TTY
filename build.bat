3fe3f8e2da0fd4b2a622b8eb556a273b65d04cca
revoconner <oconnerrev@gmail.com>
2026-09-21 00:08:07 +0530

    oldwre doc

--- build.bat
+++ build.bat
@@ -9,14 +9,9 @@ if %ERRORLEVEL% NEQ 0 (
 )
 
 echo Building server...
-clang++ -O3 -Wall -Wextra -std=c++17 -fno-exceptions -I include -o htty-server.exe src/pty.cpp src/session.cpp src/main.cpp resources/app.res -static -luser32 -lshell32 -ladvapi32 -Wl,/SUBSYSTEM:WINDOWS -Wl,/ENTRY:mainCRTStartup
-
+clang++ -O3 -Wall -Wextra -std=c++17 -fno-exceptions -I include -o htty-server.exe src/pty.cpp src/session.cpp src/main.cpp resources/app.res -static -ladvapi32 -Wl,/SUBSYSTEM:WINDOWS -Wl,/ENTRY:mainCRTStartup
 if %ERRORLEVEL%==0 echo Build successful
 
 echo Building front end...
-clang++ -O3 -Wall -Wextra -std=c++17 -fno-exceptions -I include -o headless-tty.exe src/htty.cpp src/client.cpp -static
-if %ERRORLEVEL%==0 echo Build successful
-
-echo Building helper...
-g++ -std=c++23 -o messenger.exe Helper/messenger.cpp -static -s -mwindows -lbcrypt
+clang++ -O3 -Wall -Wextra -std=c++17 -fno-exceptions -I include -o headless-tty.exe src/htty.cpp src/client.cpp resources/app.res -static
 if %ERRORLEVEL%==0 echo Build successful
